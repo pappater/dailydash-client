d088036ea693008b35c61f07236c51138a6df5a1
pratheesheswaran <pratheesheswaran@gmail.com>
2024-09-03 20:15:16 +0530

    Added jest unit tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom", // Ensure this is correct
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
